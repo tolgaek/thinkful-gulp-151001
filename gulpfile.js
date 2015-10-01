8087fe069d07d0fb1f60c252f257f573d08a060d
Tolga Ekmen <tolga.ekmen@gmail.com>
2015-10-01 18:27:13 -0400

    Implement minizeImages gulp task

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+
+gulp.task('minimizeImages', function() {
+  return gulp
+          .src('img/*.gif')
+          .pipe(imagemin())
+          .pipe(gulp.dest('build'));
+});  
